f0e265a265a1bc8001260000d90ef90ce302ebf6
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-02-03 23:18:58 +0330

    Feat : added pages into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import Products from "./pages/Products";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
